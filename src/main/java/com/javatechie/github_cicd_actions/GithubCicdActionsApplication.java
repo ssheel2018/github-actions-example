6d57c6d8a865b3cf1d7d5980ef31dc57ff916dbc
Saurabh Sheel <sheel.gta@gmail.com>
2026-05-05 23:53:41 -0400

    initial commit

---
+++ src/main/java/com/javatechie/github_cicd_actions/GithubCicdActionsApplication.java
@@ -0,0 +1,32 @@
+package com.javatechie.github_cicd_actions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class GithubCicdActionsApplication {
+
+	@GetMapping("/welcome")
+	public String helloGreeting() {
+		System.out.println("Say Hello");
+		return "welcome";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubCicdActionsApplication.class, args);
+	}
+
+}
+
+/*
+echo "# github-actions-example" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/ssheel2018/github-actions-example.git
+git push -u origin main
+ */
