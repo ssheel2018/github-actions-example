6216c385e6bbd028e5f7f2076412cb03bca7a309
Saurabh Sheel <sheel.gta@gmail.com>
2026-05-06 00:19:44 -0400

    code changes v3

--- src/main/java/com/javatechie/github_cicd_actions/GithubCicdActionsApplication.java
+++ src/main/java/com/javatechie/github_cicd_actions/GithubCicdActionsApplication.java
@@ -26,7 +26,7 @@ echo "# github-actions-example" >> README.md
 git init
 git add README.md
 git commit -m "first commit"
-git branch -M main
+git branch -M master
 git remote add origin https://github.com/ssheel2018/github-actions-example.git
-git push -u origin main
+git push -u origin master
  */
